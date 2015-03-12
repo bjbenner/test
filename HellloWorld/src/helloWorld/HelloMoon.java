e2ac8621a39192617f66f8207b8bb50a13d832a7
bjbenner <bjoern.benner@stud.uni-due.de>
2015-03-12 12:19:28 +0100

    Only commiting class "HelloMoon.java"

--- HellloWorld/src/helloWorld/HelloMoon.java
+++ HellloWorld/src/helloWorld/HelloMoon.java
@@ -4,7 +4,15 @@ public class HelloMoon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Moon");
+		HelloMoon hw = new HelloMoon();
+		hw.sayHello();
 	}
 
+	public void sayHello(){
+		this.saySomehing("Hello Moon");
+	}
+	
+	public void saySomehing(String message){
+		System.out.println(message);
+	}
 }
