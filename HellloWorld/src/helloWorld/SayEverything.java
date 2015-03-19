b21ac4b1918598f2a891ac70a3ccbfd98b5e6520
bjbenner <bjoern.benner@stud.uni-due.de>
2015-03-19 12:17:27 +0100

    Commit via console

--- HellloWorld/src/helloWorld/SayEverything.java
+++ HellloWorld/src/helloWorld/SayEverything.java
@@ -3,9 +3,10 @@ package helloWorld;
 public class SayEverything {
 
 	public static void main(String[] args) {
-		Hello[] h = new Hello[2];
+		Hello[] h = new Hello[3];
 		h[0] = new HelloWorld();
 		h[1] = new HelloMoon();
+		h[2] = new HelloSun();
 		
 		for (Hello hello : h) {
 			hello.sayHello();
